703a901b578cd3cec539d089832bb2e6fad38f59
Alex SaM <alsam3637@gmail.com>
2025-11-29 20:15:32 -0500

    Develop the NotFoundController

--- index.php
+++ index.php
@@ -2,3 +2,12 @@
 
 require __DIR__ . '/inc/all.inc.php';
 
+$page = @(string) ($_GET['page'] ?? 'index');
+
+if ($page === 'index') {
+    echo "TODO: Develop the index page!<br />\n";
+}
+else {
+    $notFoundController = new \App\Frontend\Controller\NotFoundController();
+    $notFoundController->error404();
+}
