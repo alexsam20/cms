9cf9dc97c6ee3bdb6c699dd73b7cfee73b2fc7ae
Alex SaM <alsam3637@gmail.com>
2025-12-01 22:37:39 -0500

    Container Pattern.

--- inc/db-connect.inc.php
+++ inc/db-connect.inc.php
@@ -1,13 +1,14 @@
 <?php
-/*
+
 try {
-    $pdo = new PDO('mysql:host=localhost;dbname=cms;charset=utf8mb4', 'cms', '', [
+    $pdo = new PDO('mysql:host=localhost;dbname=udemy_cms', 'alex', 'alex1970MD3214', [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 }
 catch (PDOException $e) {
     // var_dump($e->getMessage());
-    echo 'A problem occured with the database connection...';
+    echo 'A problem occurred with the database connection...';
     die();
 }
-*/
+
+return $pdo;
--- index.php
+++ index.php
@@ -1,24 +1,36 @@
 <?php
 
-require __DIR__ . '/inc/all.inc.php';
+require __DIR__.'/inc/all.inc.php';
+
+$container = new \App\Support\Container();
+$container->bind('pdo', function () {
+    return require __DIR__.'/inc/db-connect.inc.php';
+});
+$container->bind('pagesRepository', function () use ($container) {
+    $pdo = $container->get('pdo');
+    return new \App\Repository\PagesRepository($pdo);
+});
+$container->bind('pagesController', function () use ($container) {
+    $pagesRepository = $container->get('pagesRepository');
+    return new \App\Frontend\Controller\PagesController(
+        $pagesRepository
+    );
+});
+$container->bind('notFoundController', function () use ($container) {
+    $pagesRepository = $container->get('pagesRepository');
+    return new \App\Frontend\Controller\NotFoundController(
+        $pagesRepository
+    );
+});
 
 $route = @(string) ($_GET['route'] ?? 'pages');
 
 if ($route === 'pages') {
     $page = @(string) ($_GET['page'] ?? 'index');
 
-    $pagesRepository = new \App\Repository\PagesRepository($pdo);
-
-    $pagesController = new \App\Frontend\Controller\PagesController(
-        $pagesRepository
-    );
+    $pagesController = $container->get('pagesController');
     $pagesController->showPage($page);
-}
-else {
-    $pagesRepository = new \App\Repository\PagesRepository($pdo);
-
-    $notFoundController = new \App\Frontend\Controller\NotFoundController(
-        $pagesRepository
-    );
+} else {
+    $notFoundController = $container->get('notFoundController');
     $notFoundController->error404();
 }
