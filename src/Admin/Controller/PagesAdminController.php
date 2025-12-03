4ac0a60e7a7b831b1ea0b9e384696b330670ef27
Alex SaM <alsam3637@gmail.com>
2025-12-02 21:21:24 -0500

    Add Input Validation and Error Handling to the Create Form.

--- src/Admin/Controller/PagesAdminController.php
+++ src/Admin/Controller/PagesAdminController.php
@@ -20,18 +20,33 @@ class PagesAdminController extends AbstractAdminController
 
     public function create()
     {
+        $errors = [];
         if (!empty($_POST)) {
             $title = @(string) ($_POST['title'] ?? '');
             $slug = @(string) ($_POST['slug'] ?? '');
             $content = @(string) ($_POST['content'] ?? '');
 
+            $slug = strtolower($slug);
+            $slug = str_replace(['/', ' ', '.'], ['-', '-', '-'], $slug);
+            $slug = preg_replace('/[^a-z0-9\-]/', '', $slug);
+
             if (!empty($title) && !empty($slug) && !empty($content)) {
                 $slugExists = $this->pagesRepository->getSlugExists($slug);
                 if (empty($slugExists)) {
                     $this->pagesRepository->create($title, $slug, $content);
+                    header("Location: index.php?route=admin/pages");
+                    return;
+                }
+                else {
+                    $errors[] = 'Slug already exists!';
                 }
             }
+            else {
+                $errors[] = 'Are all fields filled out?';
+            }
         }
-        $this->render('pages/create', []);
+        $this->render('pages/create', [
+            'errors' => $errors
+        ]);
     }
 }
